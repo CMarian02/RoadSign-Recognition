d68a95c597c5692f93c8df8e43702cddb0ef0b4b
CMarian02 <lcmarian.office@gmail.com>
2024-10-14 23:31:49 +0300

    feat(dataset): added more images for training

--- scripts/test_yolo.py
+++ scripts/test_yolo.py
@@ -1,9 +1,11 @@
 from ultralytics import YOLO
 
-# Încarcă modelul pre-antrenat YOLOv8
-model = YOLO("C:/Users/CMarian/Desktop/Road Sign Recogniton/runs/detect/train/weights/best.pt")  # poți folosi și 'yolov8s.pt', 'yolov8m.pt' etc., în funcție de dimensiunea modelului
+model = YOLO('yolo11l.pt') 
 
-# Antrenează modelul pe datasetul tău personalizat
-# model.train(data="C:/Users/CMarian/Desktop/test/YOLODataset/dataset.yaml", epochs=50)
+# model.train(data="config.yaml", epochs=80, resume=True)
 
-model.predict("53.png")
+# results = model.predict("C:/Users/CMarian/Desktop/PP/RoadSign-Recognition/dataset/all-signs/images/Zona_cu_viteza_limitata_la_30_kmh.png", save=True, show=True)
+
+
+# Afișează rezultatele
+# print(results)
